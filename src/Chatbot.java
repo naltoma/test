1933a7eef3e1f71addef47f7985c61b226a7202b
Naruaki TOMA <tnal@ie.u-ryukyu.ac.jp>
2020-10-28 15:38:45 +0900

    done for ex2_8

--- src/Chatbot.java
+++ src/Chatbot.java
@@ -11,4 +11,11 @@ public class Chatbot{
     public void greeting(){
         System.out.println("はじめまして" + this.name + "です。よろ！");
     }
+
+    public void setName(String _name){
+        this.name = _name;
+    }
+    public String getName(){
+        return this.name;
+    }
 }
